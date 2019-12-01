5f08f91deaf23c539cf512f659f168ec6c2430d7
wolfi <eglik@odin.vision>
2019-12-01 19:28:11 +0900

    test: TextureImporter sprite split

---
+++ Assets/AsepriteAnimator/Scripts/AsepriteData.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace AsepriteAnimator
+{
+    public class AsepriteData
+    {
+        private int x;
+        private int y;
+        private int w;
+        private int h;
+
+        private string name;
+
+        public Vector2 GetPosition()
+        {
+            return new Vector2(x, y);
+        }
+
+        public Vector2 GetSize()
+        {
+            return new Vector2(w, h);
+        }
+
+        public AsepriteData(int x, int y, int w, int h)
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+
+        public override string ToString()
+        {
+            return $"x : {x}, y : {y}, w : {w}, h : {h}";
+        }
+    }
+}
--- Assets/AsepriteAnimator/Scripts/Editor/AnimatorLoader.cs
+++ Assets/AsepriteAnimator/Scripts/Editor/AnimatorLoader.cs
@@ -1,7 +1,9 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using Newtonsoft.Json.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro.SpriteAssetUtilities;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,7 +12,7 @@ namespace AsepriteAnimator
     public class AnimatorLoader : EditorWindow
     {
         private static AnimatorLoader windowInstance = null;
-        private Sprite spriteSheet;
+        private Texture spriteSheet;
         private TextAsset sheetJson;
 
         [MenuItem("Aseprite/Load Animation")]
@@ -27,25 +29,48 @@ namespace AsepriteAnimator
 
         private void OnGUI()
         {
-            spriteSheet = (Sprite)EditorGUILayout.ObjectField("Sprite Sheet", spriteSheet, typeof(Sprite), false);
+            spriteSheet = (Texture)EditorGUILayout.ObjectField("Sprite Sheet", spriteSheet, typeof(Texture), false);
             sheetJson = (TextAsset)EditorGUILayout.ObjectField("Sheet Json", sheetJson, typeof(TextAsset), false);
 
-            JToken token = JObject.Parse(sheetJson.text);
-
-            int q = 0;
-
-            List<string> test = new List<string>();
-
-            foreach (var t in token["frames"])
+            if (GUILayout.Button("Generator"))
             {
-                test.Add(t);
-                // test.Add(t.Value<string>().ToString());
-                // Debug.Log(q.ToString() + " /// x : " + root["frame"]["x"].ToString() + ", y : " + root["frame"]["y"].ToString());
-            }
+                JToken token = JObject.Parse(sheetJson.text);
 
-            foreach(var t in test)
-            {
-                Debug.Log(t);
+                int q = 0;
+
+                List<AsepriteData> test = new List<AsepriteData>();
+
+                foreach (var t in token["frames"])
+                {
+                    test.Add(new AsepriteData(int.Parse(t.Last["frame"]["x"].ToString()),
+                                              int.Parse(t.Last["frame"]["y"].ToString()),
+                                              int.Parse(t.Last["frame"]["w"].ToString()),
+                                              int.Parse(t.Last["frame"]["h"].ToString())));
+                }
+
+                TextureImporter ti = TextureImporter.GetAtPath(AssetDatabase.GetAssetPath(spriteSheet)) as TextureImporter;
+                ti.spriteImportMode = SpriteImportMode.Multiple;
+
+                List<SpriteMetaData> meta = new List<SpriteMetaData>();
+
+                int oko = 0;
+
+                foreach (var t in test)
+                {
+                    meta.Add(new SpriteMetaData()
+                    {
+                        border = Vector4.zero,
+                        rect = new Rect(t.GetPosition(), t.GetSize()),
+                        name = $"{oko} sprite.png",
+                        alignment = 0
+                    });
+
+                    oko++;
+                }
+
+                ti.isReadable = true;
+                ti.spritesheet = meta.ToArray();
+                ti.SaveAndReimport();
             }
         }
     }
