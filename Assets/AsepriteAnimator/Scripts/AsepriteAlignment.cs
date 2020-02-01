3bd29af80c3f8d7c72ad05bc39c4c8a007688288
wolfi <eglik@odin.vision>
2020-02-01 13:24:36 +0900

    feat: Add README

--- Assets/AsepriteAnimator/Scripts/AsepriteAlignment.cs
+++ Assets/AsepriteAnimator/Scripts/AsepriteAlignment.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum AsepriteAlignment
+namespace AsepriteAnimator
 {
-    Center = 0,
-    TopLeft = 1,
-    TopCenter = 2,
-    TopRight = 3,
-    LeftCenter = 4,
-    RightCenter = 5,
-    BottomLeft = 6,
-    BottomCenter = 7,
-    BottomRight = 8
+    public enum AsepriteAlignment
+    {
+        Center = 0,
+        TopLeft = 1,
+        TopCenter = 2,
+        TopRight = 3,
+        LeftCenter = 4,
+        RightCenter = 5,
+        BottomLeft = 6,
+        BottomCenter = 7,
+        BottomRight = 8
+    }
 }
