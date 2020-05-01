e7c6b46a443be4bffdc816bcc2ba2498f8c0c714
wolfi <wolfi@bytenine.io>
2020-05-01 12:38:16 +0900

    test: ScriptedImporter

---
+++ Assets/AsepriteAnimator/Scripts/AsepriteImporter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+public class AsepriteImporter : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        throw new System.NotImplementedException();
+    }
+}
